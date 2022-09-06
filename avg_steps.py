5993142e16927787e93c7325aecbb753bb9bcff0
Matthew <mattmccullough777@gmail.com>
2022-09-06 11:47:51 -0500

    commit

---
+++ avg_steps.py
@@ -0,0 +1,14 @@
+def main():
+    import csv
+
+    infile = open("steps.csv","r")
+
+    csvfile = csv.reader(infile, delimiter=",")
+
+    for record in csvfile:
+        month = (record[0]) #this is the number value for month from steps.csv
+
+    if month 
+        
+
+main()
